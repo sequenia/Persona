793bd3a4a2e16a63ef0234cbbb733e6881a3bc11
Базов Петр <petr.sequenia.com>
2013-09-10 17:27:36 +0700

    One Page diz

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -42,11 +42,9 @@ class PeopleController < ApplicationController
   def update
     respond_to do |format|
       if @person.update(person_params)
-        format.html { redirect_to @person, notice: 'Person was successfully updated.' }
-        format.json { head :no_content }
+        format.html { redirect_to people_path, notice: 'Person was successfully updated.' }
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @person.errors, status: :unprocessable_entity }
+        format.html { redirect_to people_path, notice: 'Error.' }
       end
     end
   end
